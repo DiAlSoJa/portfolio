16476aed307164007f6094c1ab65d26824234485
DiAlSoJa <sotodiego37@gmail.com>
2024-07-18 07:40:48 -0600

    subi deste de vercel

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -1,18 +1,18 @@
-import { initializeApp,getApps,getApp} from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { initializeApp,getApps,getApp} from "firebase/app";
+// import { getAuth } from "firebase/auth";
 
 
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-  };
+// const firebaseConfig = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     projectId: process.env.PROJECT_ID,
+//     storageBucket: process.env.STORAGE_BUCKET,
+//     messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//     appId: process.env.APP_ID,
+//     measurementId: process.env.MEASUREMENT_ID
+//   };
 
-const app = !getApps().length? initializeApp(firebaseConfig):getApp();
+// const app = !getApps().length? initializeApp(firebaseConfig):getApp();
 
-const auth = getAuth(app);
-export {app,auth};
+// const auth = getAuth(app);
+// export {app,auth};
